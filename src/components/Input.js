db07ecc32093d9f2eb23c557056def89861b1bc6
abdelrahmanayaad <abdelrahmanayad74@gmail.com>
2022-09-28 21:02:28 +0200

    Hello Validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import {View, Text, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import {COLORS, FONTS} from '../constants/Constants';
 import {RFValue} from 'react-native-responsive-fontsize';
 
 function Input(props) {
-  const {underline, bordered} = props;
+  const {
+    underline,
+    bordered,
+    isValid,
+    feedback,
+    touched,
+    style,
+    password,
+    ...rest
+  } = props;
   return (
     <View
-      style={{backgroundColor: '#eee', borderRadius: 5, paddingHorizontal: 5}}>
-      <TextInput {...props} placeholderTextColor={COLORS.dark_gray} />
+      style={[
+        {
+          backgroundColor: '#eee',
+          borderRadius: 5,
+          paddingHorizontal: 5,
+          flexDirection: 'row',
+          alignItems: 'center',
+          borderWidth: 1,
+        },
+        isValid
+          ? {borderColor: 'green'}
+          : {borderColor: touched ? 'red' : '#eee'},
+      ]}>
+      <TextInput
+        style={[{flex: 1}, style]}
+        {...rest}
+        placeholderTextColor={COLORS.dark_gray}
+      />
       {underline && (
         <View style={{height: RFValue(1), backgroundColor: COLORS.black}} />
+      )}
+      {feedback && touched && (
+        <Icon
+          name={isValid ? 'check' : 'close'}
+          size={20}
+          style={isValid ? {color: 'green'} : {color: 'red'}}
+        />
+      )}
+      {password && (
+        <Icon
+          name={isValid ? 'eye' : 'eyeo'}
+          size={20}
+          style={{color: '#000'}}
+        />
       )}
     </View>
   );
--- src/screens/Test/index.js
+++ src/screens/Test/index.js
@@ -8,10 +8,24 @@ function Test(props) {
   // Values in TextInput
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  //Icons
+  const [emailIcon, setEmailIcon] = useState(false);
+  const [phoneIcon, setPhoneIcon] = useState(false);
+
+  //touched
+  const [emailTouch, setEmailTouch] = useState(false);
+  const [phoneTouch, setPhoneTouch] = useState(false);
+  const [passwordTouch, setpasswordTouch] = useState(false);
+  const [confirmPasswordTouch, setconfirmPasswordTouch] = useState(false);
 
   //errors
   const [emailError, setEmailError] = useState('');
   const [phoneError, setPhoneError] = useState('');
+  const [passwordError, setpasswordError] = useState('');
+  const [confirmPasswordError, setconfirmPasswordError] = useState('');
 
   //function of inputs validation
   function validate() {
@@ -22,25 +36,68 @@ function Test(props) {
       /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
     if (email.trim().length == 0) {
       setEmailError('Must Enter Email');
+      setEmailIcon(false);
+      setEmailTouch(true);
       error++;
     } else if (regEmail.test(email.trim()) == false) {
       setEmailError('Enter Correct Email');
+      setEmailIcon(false);
+      setEmailTouch(true);
       error++;
     } else {
       setEmailError('');
+      setEmailIcon(true);
+      setEmailTouch(true);
     }
 
     //phone
     let reg = /^(\+201|01){1}[0-2,5][0-9]{8}$/;
     if (phone.trim().length == 0) {
       setPhoneError('Must enter phone');
+      setPhoneIcon(false);
+      setPhoneTouch(true);
       error++;
     } else if (reg.test(phone.trim()) == false) {
       setPhoneError('Enter correct phone');
+      setPhoneIcon(false);
+      setPhoneTouch(true);
       error++;
     } else {
       setPhoneError('');
+      setPhoneIcon(true);
+      setPhoneTouch(true);
     }
+
+    //password
+
+    if (password.length == 0) {
+      setpasswordError('Must enter Password');
+      setpasswordTouch(true);
+      error++;
+    } else if (password == false) {
+      setpasswordError('Enter correct Password');
+      setpasswordTouch(true);
+      error++;
+    } else {
+      setpasswordError('');
+      setpasswordTouch(true);
+    }
+
+    //password
+
+    if (confirmPassword.length == 0) {
+      setconfirmPasswordError('Must enter confirm Password');
+      setconfirmPasswordTouch(true);
+      error++;
+    } else if (confirmPassword !== password) {
+      setconfirmPasswordError('confirm password not match');
+      setconfirmPasswordTouch(true);
+      error++;
+    } else {
+      setpasswordError('');
+      setpasswordTouch(true);
+    }
+
     if (error == 0) {
       alert('Hello Validation');
     }
@@ -48,12 +105,48 @@ function Test(props) {
 
   function onChangeEmail(value) {
     const reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    return reg.test(value.trim());
+    if (reg.test(value.trim()) == true) {
+      setEmailIcon(true);
+      setEmailTouch(true);
+      return true;
+    } else {
+      setEmailIcon(false);
+      setEmailTouch(true);
+      return false;
+    }
   }
 
   const onChangePhone = value => {
-    let reg = /^(\+201|01){1}[0-2,5][0-9]{8}$/;
-    return reg.test(value.trim());
+    const reg = /^(\+201|01){1}[0-2,5][0-9]{8}$/;
+    if (reg.test(value.trim()) == true) {
+      setPhoneIcon(true);
+      setPhoneTouch(true);
+      return true;
+    } else {
+      setPhoneIcon(false);
+      setPhoneTouch(true);
+      return false;
+    }
+  };
+
+  const onChangePassword = value => {
+    if (password.length < 6) {
+      setpasswordTouch(true);
+      return true;
+    } else {
+      setpasswordTouch(true);
+      return false;
+    }
+  };
+
+  const onChangeConfirmPassword = value => {
+    if (confirmPassword.length < 6 || confirmPassword != password) {
+      setconfirmPasswordTouch(true);
+      return true;
+    } else {
+      setconfirmPasswordTouch(true);
+      return false;
+    }
   };
 
   return (
@@ -62,11 +155,14 @@ function Test(props) {
         <Text style={styles.title}>Register</Text>
         <Text style={styles.msg}>Enter your deatils to register</Text>
 
+        {/* TextInput 1 */}
         <View style={styles.inputs}>
-          {/* TextInput 1 */}
           <Text style={styles.placeholderTitle}>Email</Text>
           <View>
             <Input
+              feedback
+              isValid={emailIcon}
+              touched={emailTouch}
               style={styles.input}
               bordered
               placeholder={'Enter your email'}
@@ -81,11 +177,14 @@ function Test(props) {
           </View>
         </View>
 
+        {/* TextInput 2 */}
         <View style={styles.inputs}>
-          {/* TextInput 2 */}
           <Text style={styles.placeholderTitle}>Phone</Text>
           <View>
             <Input
+              feedback
+              isValid={phoneIcon}
+              touched={phoneTouch}
               maxLength={11}
               style={styles.input}
               bordered
@@ -98,6 +197,48 @@ function Test(props) {
               }}
             />
             <Text style={{color: 'red'}}>{phoneError}</Text>
+          </View>
+        </View>
+
+        {/*TextInput 3*/}
+        <View style={styles.inputs}>
+          <Text style={styles.placeholderTitle}>Password</Text>
+          <View>
+            <Input
+              password
+              secureTextEntry={true}
+              style={styles.input}
+              bordered
+              placeholder={'Enter Password'}
+              onChangeText={value => {
+                setPassword(value);
+                if (onChangePassword(value)) {
+                  setpasswordError('');
+                }
+              }}
+            />
+            <Text style={{color: 'red'}}>{passwordError}</Text>
+          </View>
+        </View>
+
+        {/*TextInput 4*/}
+        <View style={styles.inputs}>
+          <Text style={styles.placeholderTitle}>Confirm Password</Text>
+          <View>
+            <Input
+              password
+              secureTextEntry={true}
+              style={styles.input}
+              bordered
+              placeholder={'Confirm Password'}
+              onChangeText={value => {
+                setConfirmPassword(value);
+                if (onChangeConfirmPassword(value)) {
+                  setconfirmPasswordError('');
+                }
+              }}
+            />
+            <Text style={{color: 'red'}}>{confirmPasswordError}</Text>
           </View>
         </View>
 
