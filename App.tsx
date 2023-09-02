3c0e41e18a2cf988b673fa8e9debbebbf418535d
abdelrahmanayaad <abdelrahmanayad74@gmail.com>
2023-09-02 03:18:33 +0300

    React Native Code

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Abdelrahman Ayad</Text>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default App
